16d3e72a5d1f3389a001ccaa3492d7e27d2c6f35
ncdhz <1137436221@qq.com>
2022-09-29 23:25:00 +0800

    add code

---
+++ code/507.完美数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=507 lang=javascript
+ *
+ * [507] 完美数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+
+};
+// @lc code=end
+
