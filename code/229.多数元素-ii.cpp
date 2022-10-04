d12da5ea0d4381c84042591422e9c33b763d5810
ncdhz <1137436221@qq.com>
2022-10-04 23:24:20 +0800

    add code

--- code/229.多数元素-ii.cpp
+++ code/229.多数元素-ii.cpp
@@ -8,7 +8,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-
+        int element1;
+        int element2;
+        int v1 = 0;
+        int v2 = 0;
+        for (int num: nums) {
+            if (v1 > 0 && num == element1) {
+                v1++;
+            } else if (v2 > 0 && num == element2) {
+                v2++;
+            } else if (v1 == 0) {
+                element1 = num;
+                v1++;
+            } else if (v2 == 0) {
+                element2 = num;
+                v2++;
+            } else {
+                v1--;
+                v2--;
+            }
+        }
+        int vv1 = 0;
+        int vv2 = 0;
+        for (int num: nums) {
+            if (v1 > 0 && num == element1) {
+                vv1++;
+            } else if (v2 > 0 && num == element2) {
+                vv2++;
+            }
+        }
+        vector<int> rs;
+        if (v1 > 0 && vv1 > nums.size() / 3) {
+            rs.push_back(element1);
+        }
+        if (v2 > 0 && vv2 > nums.size() / 3) {
+            rs.push_back(element2);
+        }
+        return rs;
     }
 };
 // @lc code=end
