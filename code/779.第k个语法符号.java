684b94505b869e7ddff1ea7b4024f0d7f8c10706
ncdhz <1137436221@qq.com>
2022-10-20 23:22:53 +0800

    add code

---
+++ code/779.第k个语法符号.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=779 lang=java
+ *
+ * [779] 第K个语法符号
+ */
+
+// @lc code=start
+class Solution {
+    public int kthGrammar(int n, int k) {
+        // 0
+        // 01
+        // 0110
+        // 01 10 10 01
+        // 01 10 10 01 10 01 01 10
+    }
+}
+// @lc code=end
+
