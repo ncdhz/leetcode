77bbc4c8d17f998b08b16e836325c27715d8343c
ncdhz <1137436221@qq.com>
2022-11-14 22:37:52 +0800

    add code

--- code/791.自定义字符串排序.cpp
+++ code/791.自定义字符串排序.cpp
@@ -3,12 +3,46 @@
  *
  * [791] 自定义字符串排序
  */
+#include <map>
+#include <set>
+#include <sstream>
+using namespace std;
 
 // @lc code=start
 class Solution {
 public:
     string customSortString(string order, string s) {
-
+        map<int, char> ms;
+        map<char, int> mss;
+        set<char> ss;
+        stringstream cs;
+        stringstream cs1;
+        for (size_t i = 0; i < order.size(); i++) {
+            ss.insert(order[i]);
+            ms.insert(make_pair(i, order[i]));
+        }
+        
+        for (size_t i = 0; i < s.size(); i++) {
+            if (ss.count(s[i]) == 0) {
+                cs << s[i];
+                continue;
+            }
+            if (mss.count(s[i]) > 0) {
+                mss[s[i]] = mss[s[i]] + 1;
+            } else {
+                mss.insert(make_pair(s[i], 1));
+            }
+        }
+        
+        for (size_t i = 0; i < order.size(); i++) {
+            if (mss.count(ms[i]) > 0) {
+                for (size_t k = 0; k < mss[ms[i]]; k++) {
+                    cs1 << ms[i];
+                }
+            }
+        }
+        
+        return cs1.str() + cs.str();
     }
 };
 // @lc code=end
