5eb168eb6e0e3c92b398e5b8eacbbdf81da45df9
ncdhz <1137436221@qq.com>
2024-05-27 17:18:13 +0800

    update

--- code/746.使用最小花费爬楼梯.py
+++ code/746.使用最小花费爬楼梯.py
@@ -7,6 +7,9 @@
 # @lc code=start
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-       
+        m1 = cost[1]
+        m2 = min([m1, cost[0]])
+        for c in cost[2:]:
+            m2, m1 = min([m2 + c, m1]), m2 + c
+        return m2
 # @lc code=end
-
